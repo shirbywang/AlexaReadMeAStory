a95030d26e634e99d48bd85a076cfbea11a4a748
Lawrence Vo <lawrencetvo@gmail.com>
2017-03-04 15:57:28 -0800

    got rid of http server

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const http = require('http');
 const bodyParser = require('body-parser');
 
 const app = express();
-const server = http.createServer(app);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(3000);
+app.listen(3000);
