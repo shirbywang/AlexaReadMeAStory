1e6b968b4b7d8ad68eba1a4a31cd4153ccc54f45
Julien Farbarik <julien.farbarik@gmail.com>
2017-03-05 08:15:41 -0800

    Attempting to interface with Wolfram Alpha

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
-const googleAPIs = require('googleapis')
+const googleAPIs = require('googleapis');
+const axios = require('axios');
+const $ = require('jquery');
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
+	readFromImage("http://i.imgur.com/y8ha1ld.jpg");
   res.status(200).send('Hello World!');
 });
 
@@ -24,3 +28,16 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log('It works!');
 });
+
+
+function readFromImage(imageFile){
+
+	axios.post('https://www.wolframcloud.com/objects/8f903053-9f4c-4200-8c8b-ee40d88765d3/?name='+imageFile)
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+};
+
