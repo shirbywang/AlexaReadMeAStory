e37ff3b5d9107a404037fdc77ef9ea62ffb01342
Julien Farbarik <julien.farbarik@gmail.com>
2017-03-05 09:45:45 -0800

    interface for twilio

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const bodyParser = require('body-parser');
 const googleAPIs = require('googleapis');
 const axios = require('axios');
 const $ = require('jquery');
-
+theRealStory = "";
+imagelink = "";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  const imageFile = "http://i.imgur.com/y8ha1ld.jpg";
-	const url = 'https://www.wolframcloud.com/objects/902ee9ce-e012-44b7-b1c5-444f56fd0d95/?name='+imageFile;
+  res.send("DNE");
+  const testImage = "http://i.imgur.com/y8ha1ld.jpg";
+	const url = 'https://www.wolframcloud.com/objects/902ee9ce-e012-44b7-b1c5-444f56fd0d95/?name='+imagelink;
 	axios.post(url)
     .then(function (response) {
       res.send(response.data);
@@ -19,6 +21,18 @@ app.get('/', (req, res) => {
     .catch(function (error) {
       res.status(200).send("ERROR");
 		});
+});
+
+app.post('/', (req, res) => {
+  //const { Body, From, MediaUrl0 } = req.body;
+  //const message = { body: Body, from: From, img: MediaUrl0 }
+  const{img} = req.body
+  imagelink = readFromImage(img)
+  res.send(`
+    <Response>
+      <Message>Thanks for texting!</Message>
+    </Response>
+  `);
 });
 
 const PORT = process.env.PORT || 8080;
