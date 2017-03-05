9c96a3e1dbd6754e220cd48aaa979ae22eea9fb6
Lawrence Vo <lawrencetvo@gmail.com>
2017-03-05 09:32:10 -0800

    got get to work

--- index.js
+++ index.js
@@ -10,36 +10,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-	readFromImage("http://i.imgur.com/y8ha1ld.jpg");
-  res.status(200).send('Hello World!');
-});
-
-app.post('/', (req, res) => {
-  //const { Body, From, MediaUrl0 } = req.body;
-  //const message = { body: Body, from: From, img: MediaUrl0 }
-  res.send(`
-    <Response>
-      <Message>Thanks for texting!</Message>
-    </Response>
-  `);
+  const imageFile = "http://i.imgur.com/y8ha1ld.jpg";
+	const url = 'https://www.wolframcloud.com/objects/902ee9ce-e012-44b7-b1c5-444f56fd0d95/?name='+imageFile;
+	axios.post(url)
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      res.status(200).send("ERROR");
+		});
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log('It works!');
 });
-
-
-function readFromImage(imageFile){
-	const url = 'https://www.wolframcloud.com/objects/902ee9ce-e012-44b7-b1c5-444f56fd0d95/?name='+imageFile;
-	console.log(url);
-	axios.post(url)
-		.then(function (response) {
-			console.log("HERE");
-			console.log(response.data);
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
-};
 
